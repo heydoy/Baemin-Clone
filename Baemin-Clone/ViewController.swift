b828d9a187f97b917f79686f825e693875e1f95a
heydoy <heydoyhey@gmail.com>
2022-07-08 09:22:26 +0900

    Initial Commit

---
+++ Baemin-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  02-SesacAssignment-Baemin
+//
+//  Created by Doy Kim on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
